3910cba81aa2d986af21da5cfb049cf9a5d7aa18
pypalkar23 <mandar.palkar94@gmail.com>
2021-12-14 12:13:51 -0500

    Deleting old directory

--- frontend/src/components/Follow/Follow.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Follow from './Follow';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Follow />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
